ca5ee2411d3bc2c69b5e23c50c796116db957636
DanielPaque <d_paque12@cs.uni-kl.de>
2016-11-01 11:17:24 +0100

    Commented out some error lines in messagin service -.-

--- messaging-impl/src/main/java/org/lagomy/message/impl/MessageEntity.java
+++ messaging-impl/src/main/java/org/lagomy/message/impl/MessageEntity.java
@@ -18,23 +18,23 @@ public class MessageEntity extends PersistentEntity<MessageCommand, MessageEvent
         BehaviorBuilder b = newBehaviorBuilder(snapshotState.orElse(
                 new MessageState()));
 
-        b.setCommandHandler(MessageCommand.SendMessage.class, (cmd, ctx) -> {
-            if (state().message.isPresent()) {
-                ctx.invalidCommand("Message " + entityId() + " is already created");
-                return ctx.done();
-            } else {
-                Message message = cmd.message;
-                List<MessageEvent> events = new ArrayList<MessageEvent>();
-                events.add(new MessageEvent.MessageSent(message.source, message.message,message.receiver));
-//                for (String friendId : user.friends) {
-//                    events.add(new FriendAdded(user.userId, friendId));
-//                }
-                return ctx.thenPersistAll(events, () -> ctx.reply(Done.getInstance()));
-            }
-        });
-
-        b.setEventHandler(MessageEvent.MessageSent.class,
-                evt -> new MessageState(Optional.of(new Message(evt.source, evt.message, evt.receiver))));
+//        b.setCommandHandler(MessageCommand.SendMessage.class, (cmd, ctx) -> {
+//            if (state().message.isPresent()) {
+//                ctx.invalidCommand("Message " + entityId() + " is already created");
+//                return ctx.done();
+//            } else {
+//                Message message = cmd.message;
+//                List<MessageEvent> events = new ArrayList<MessageEvent>();
+//                events.add(new MessageEvent.MessageSent(message.source, message.message,message.receiver));
+////                for (String friendId : user.friends) {
+////                    events.add(new FriendAdded(user.userId, friendId));
+////                }
+//                return ctx.thenPersistAll(events, () -> ctx.reply(Done.getInstance()));
+//            }
+//        });
+//
+//        b.setEventHandler(MessageEvent.MessageSent.class,
+//                evt -> new MessageState(Optional.of(new Message(evt.source, evt.message, evt.receiver))));
 
 
 
